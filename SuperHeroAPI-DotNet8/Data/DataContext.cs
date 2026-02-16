e66fd2de8dc9932c1ff714105a8f1cf282965071
Niklas89 <niklas_edelstam@hotmail.com>
2026-02-16 23:38:05 +0100

    first commit dotnet 8 project

---
+++ SuperHeroAPI-DotNet8/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI_DotNet8.Entities;
+
+namespace SuperHeroAPI_DotNet8.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
---
+++ SuperHeroAPI-DotNet8/Services/SuperHeroService/ISuperHeroService.cs
@@ -0,0 +1,13 @@
+﻿using SuperHeroAPI_DotNet8.Entities;
+
+namespace SuperHeroAPI_DotNet8.Services.SuperHeroService
+{
+    public interface ISuperHeroService
+    {
+        Task<List<SuperHero>> GetAllHeroes();
+        Task<SuperHero?> GetSingleHero(int id);
+        Task<List<SuperHero>> AddHero(SuperHero hero);
+        Task<List<SuperHero>?> UpdateHero(int id, SuperHero request);
+        Task<List<SuperHero>?> DeleteHero(int id);
+    }
+}
